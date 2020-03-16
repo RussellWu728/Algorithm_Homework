f1e67498f3f02dced73df885003cb0e8b94c55b7
RussellWu728 <61179463+RussellWu728@users.noreply.github.com>
2020-03-16 11:29:27 +0800

    Add files via upload

---
+++ 作业3—Search_1/Search_1.cpp
@@ -0,0 +1,31 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int Search_1(int* num,  int c,int n) {
+
+    int i ;
+    for(i=0;i<c;i++ ){
+        if (num[i] == n) {
+            return i+1;
+        }
+
+    }
+    return 0;
+
+}
+
+int main()
+{
+    int c;
+    cin >> c;
+    int* num = (int*)malloc(sizeof(int) * c);
+    for (int i = 0; i < c; i++) {
+        num[i] = i + 1;
+    }
+    int k;
+    cin >> k;
+    cout<< Search_1(num,c, k);
+}
+
