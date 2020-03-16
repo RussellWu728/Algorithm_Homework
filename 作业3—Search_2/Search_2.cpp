8876c1dd1c38fc7bffde72fee80413d58ba8efb5
RussellWu728 <61179463+RussellWu728@users.noreply.github.com>
2020-03-16 11:29:55 +0800

    Add files via upload

---
+++ 作业3—Search_2/Search_2.cpp
@@ -0,0 +1,39 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int Search_1(int* num, int c, int n) {
+    int l, r;
+    l = 0;
+    r = c-1;
+    int mid = (l + r) / 2;
+    while (1 != (r-1)) {
+        if (num[mid] > n) {
+            r = mid;
+            mid = (l + r) / 2;
+        }
+        else if (num[mid] < n) {
+            l = mid;
+            mid = (l + r) / 2;
+        }
+        else {
+            return mid + 1;
+        }
+        
+        return 0;
+    }
+}
+
+int main()
+{
+    int c;
+    cin >> c;
+    int* num = (int*)malloc(sizeof(int) * c);
+    for (int i = 0; i < c; i++) {
+        num[i] = i + 1;
+    }
+    int k;
+    cin >> k;
+    cout << Search_1(num, c, k);
+}
